a67f146d9bcc365827d0da555c624d6a56d494af
phil <philgoodmusic@gmail.com>
2020-02-27 22:52:38 -0500

    added some todos

--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -6,6 +6,11 @@ import {
     setSpendrLimit,
 } from '../../actions/accountActions';
 
+// TODO:  
+// 1.  need to refactor this page, the InputFormWithButton component doesn't make sense.  whole page should be form
+// 2.  need to wire up db schema to add start/end date for budget
+// 3.  dashboard should fetch user start/end date when component mounts then use those dates to fetch transactions
+
 const Settings = ({ userId, setSpendrLimit }) => {
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
